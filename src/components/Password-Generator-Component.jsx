7031034fdb54cc35b5775a2a7308f09e0aeba0ae
Lurgas0214 <84701652+Lurgas0214@users.noreply.github.com>
2021-09-14 10:49:10 +0200

    update all

--- src/components/Password-Generator-Component.jsx
+++ src/components/Password-Generator-Component.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "font-awesome/css/font-awesome.min.css";
 
-class PasswordGeneratorComponent extends React.Component{
+class PasswordGeneratorComponent extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
@@ -25,38 +25,38 @@ class PasswordGeneratorComponent extends React.Component{
         const e = src.split("");
         let ctr = false;
         e.forEach(c => {
-            if(dst.includes(c)) ctr = true;
+            if (dst.includes(c)) ctr = true;
         });
         return ctr;
     };
 
     isIncomplete = (passphrase) => {
-        const {digits, symbols, lowerChars, upperChars, withDigits, withSymbols, withLowerCase, withUpperCase} = this.state;
+        const { digits, symbols, lowerChars, upperChars, withDigits, withSymbols, withLowerCase, withUpperCase } = this.state;
 
-        if(withSymbols && !this.includes(symbols, passphrase)) return true;
-        if(withDigits && !this.includes(digits, passphrase)) return true;
-        if(withLowerCase && !this.includes(lowerChars, passphrase)) return true;
-        if(withUpperCase && !this.includes(upperChars, passphrase)) return true;
+        if (withDigits && !this.includes(digits, passphrase)) return true;
+        if (withSymbols && !this.includes(symbols, passphrase)) return true;
+        if (withLowerCase && !this.includes(lowerChars, passphrase)) return true;
+        if (withUpperCase && !this.includes(upperChars, passphrase)) return true;
 
         return false;
     };
 
     generatePassword = (number) => {
-        const {digits, symbols, lowerChars, upperChars, withDigits, withSymbols, withLowerCase, withUpperCase} = this.state;
+        const { digits, symbols, lowerChars, upperChars, withDigits, withSymbols, withLowerCase, withUpperCase } = this.state;
         let chars = "", result, max, i;
 
-        if(withDigits) chars += digits;
-        if(withSymbols) chars += symbols;
-        if(withLowerCase) chars += lowerChars;
-        if(withUpperCase) chars += upperChars;
+        if (withDigits) chars += digits;
+        if (withSymbols) chars += symbols;
+        if (withLowerCase) chars += lowerChars;
+        if (withUpperCase) chars += upperChars;
 
         max = chars.length;
 
-        do{
+        do {
             result = "";
-            for(i = 0; i < number; i++)
+            for (i = 0; i < number; i++)
                 result += chars[Math.floor(Math.random() * max)];
-        }while(this.isIncomplete(result));
+        } while (this.isIncomplete(result));
 
         return result;
     };
@@ -73,21 +73,21 @@ class PasswordGeneratorComponent extends React.Component{
     onClickHandle = () => {
         const value = this.state.numberOfSymbols;
 
-        if(!value) return;
+        if (!value) return;
 
-        this.setState({ password : this.generatePassword(value) })
+        this.setState({ password: this.generatePassword(value) })
     };
 
     onCheckBoxHandle = (attribute) => {
         const value = !this.state[attribute];
 
-        this.setState({[attribute]: value})
+        this.setState({ [attribute]: value })
     };
 
-    onChangeHandle = ({target}) => {
+    onChangeHandle = ({ target }) => {
         const value = target.value;
 
-        this.setState({numberOfSymbols: value})
+        this.setState({ numberOfSymbols: value })
     };
 
     render() {
@@ -97,20 +97,20 @@ class PasswordGeneratorComponent extends React.Component{
                     <div className="col">
                         <ul className="list-group">
                             <li className="list-group-item">
-                                <input type="checkbox" checked={this.state.withSymbols} onChange={()=>
-                                    this.onCheckBoxHandle("withSymbols")}/> include symbols ({this.state.symbols})
+                                <input type="checkbox" checked={this.state.withSymbols} onChange={() =>
+                                    this.onCheckBoxHandle("withSymbols")} /> include symbols ({this.state.symbols})
                             </li>
                             <li className="list-group-item">
-                                <input type="checkbox" checked={this.state.withDigits} onChange={()=>
-                                    this.onCheckBoxHandle("withDigits")}/> include digits ({this.state.digits})
+                                <input type="checkbox" checked={this.state.withDigits} onChange={() =>
+                                    this.onCheckBoxHandle("withDigits")} /> include digits ({this.state.digits})
                             </li>
                             <li className="list-group-item">
-                                <input type="checkbox" checked={this.state.withLowerCase} onChange={()=>
-                                    this.onCheckBoxHandle("withLowerCase")}/> include lower characters ({this.state.lowerChars})
+                                <input type="checkbox" checked={this.state.withLowerCase} onChange={() =>
+                                    this.onCheckBoxHandle("withLowerCase")} /> include lower characters ({this.state.lowerChars})
                             </li>
                             <li className="list-group-item">
-                                <input type="checkbox" checked={this.state.withUpperCase} onChange={()=>
-                                    this.onCheckBoxHandle("withUpperCase")}/> include upper character ({this.state.upperChars})
+                                <input type="checkbox" checked={this.state.withUpperCase} onChange={() =>
+                                    this.onCheckBoxHandle("withUpperCase")} /> include upper character ({this.state.upperChars})
                             </li>
                         </ul>
                     </div>
@@ -132,11 +132,11 @@ class PasswordGeneratorComponent extends React.Component{
                         </div>
                     </div>
                 </div>
-                <br/>
+                <br />
                 <div className="row">
                     <div className="col input-group">
                         <label className="input-group-text">Generated Password</label>
-                        <input id="password" className="form-control" type="Text" readOnly={true} value={this.state.password}/>
+                        <input id="password" className="form-control" type="Text" readOnly={true} value={this.state.password} />
                         <button className="input-group-text btn btn-outline-info" onClick={this.onCopyHandle}>copy to clipboard</button>
                     </div>
                 </div>
